62374e0ed789c38d84bf752301997343aea7559a
Tony <syntony666@outlook.com>
2021-04-25 03:18:32 +0800

    add windows bat

---
+++ depoly/deploy.bat
@@ -0,0 +1,12 @@
+call npm run build
+
+cd ./src/.vuepress/dist
+
+echo 'discord-bot.syntony666.com' > CNAME
+
+call git init
+call git add -A
+call git commit -m 'deploy'
+call git push -f https://github.com/syntony666/discord-bot-doc.git master:gh-pages
+
+cd -
