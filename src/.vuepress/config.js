72c7512a43a2997de34e624c52732ebdbc054cae
Tony <syntony666@outlook.com>
2021-04-24 16:25:23 +0800

    first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,57 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: '工具人使用手冊',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+  theme: 'default-prefers-color-scheme',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '指令說明',
+        link: '/command/'
+      },
+      {
+        text: '關於',
+        link: '/about/',
+      },
+    ],
+    sidebar: {
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/about/README.md
@@ -0,0 +1,22 @@
+---
+sidebar: false
+---
+
+## 關於工具人
+前身是tvpc-bot與lego-bot，經過多日的淬煉製作而成的
+
+[工具人邀請連結](https://discord.com/api/oauth2/authorize?client_id=785855198279106582&permissions=0&scope=bot)
+
+## 關於作者
+
+我的網站: [blog.syntony666.com](https://blog.syntony666.com)
+
+GitHub: [syntony666](https://github.com/syntony666)
+
+Discord: [邀請連結](https://discord.gg/ZS4BMESszq)
+
+## 版本紀錄
+
+### ver. 3.0.0
+#### 2021.4.24
+移植舊的tvpc-bot, lego-bot並整合大部分功能至工具人
